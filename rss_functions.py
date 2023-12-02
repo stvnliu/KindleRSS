d24da8bd7b3bd3db26de29f5ce1e0bab70b9ea7e
Zhongheng Liu <z.liu@outlook.com.gr>
2023-12-02 13:25:13 +0200

    fixed merge-induced errors

--- rss_functions.py
+++ rss_functions.py
@@ -34,9 +34,15 @@ SCIENCE_DIRECT_MAPPING = {
     SCIENCE_DIRECT_CONFIG[9]: "sources",
     SCIENCE_DIRECT_CONFIG[10]: "related"
 }
+<<<<<<< HEAD
 
 
 # Article object that abstracts a list of strings containing a representation of an article into a JSON file.
+=======
+"""
+Article object that abstracts a list of strings containing a representation of an article into a JSON file.
+"""
+>>>>>>> d2dc8b7 (fixed merge-induced errors)
 class Article:
     def __init__(self, textlines: list[str], match_keywords: list[str]) -> None:
         match_index = 0
